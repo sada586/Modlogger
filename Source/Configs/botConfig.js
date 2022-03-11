7c16dad066749d37bb14907d39fd508e20d19256
sada586 <67920767+sada586@users.noreply.github.com>
2022-03-11 19:40:50 +0300

    Add files via upload

---
+++ Source/Configs/botConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  botToken: "", // Token
+  botPrefix: "!", // Prefix
+  botPlaying: "Sleax <3 Code Share", // Oynuyor Kısmı
+  mongoDB_ConnectURL: "", // Mongodb bağlanma URL
+};
